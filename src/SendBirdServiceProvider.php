12db6e28cc6c7368d288355e38003b9ddd6408d6
rameez rami <ramees.pu@cubettech.com>
2019-08-28 16:06:51 +0530

    initial commit

---
+++ src/SendBirdServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ThinksterMinds\SendBird;
+
+use Illuminate\Support\ServiceProvider;
+
+class SendBirdServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Boot the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
